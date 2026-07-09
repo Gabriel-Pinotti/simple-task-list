11c24615f07383b03bfeddc45209643c60a1c604
gabriel-pinotti <gabriel.apinotti06@gmail.com>
2026-07-09 16:23:06 -0300

    feat: basic task mapping

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,21 @@ import { useState } from 'react'
 import type { Task } from './types/task'
 
 function App() {
-  const [taskList, setTaskList] = useState<Task[]>([])
+  const [taskList, setTaskList] = useState<Task[]>([
+    { id: 1, name: "Criar o app", completed: true} // example task
+  ])
 
   return (
-    <></>
+    <>
+      <h2>Tasks</h2>
+      <ul className='mainTaskList'>
+        {
+          taskList.map((currentTask) => (
+            <li key={currentTask.id}>{currentTask.name} | status: {currentTask.completed ? "completo" : "incompleto"}</li>
+          ))
+        }
+      </ul>
+    </>
   )
 }
 
