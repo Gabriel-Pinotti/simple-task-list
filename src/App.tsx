bc6fd862dc134e8ad554b79fc432ae53ae80a3e4
gabriel-pinotti <gabriel.apinotti06@gmail.com>
2026-07-09 17:05:19 -0300

    feat: input field logic

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,17 @@ function App() {
   const [taskList, setTaskList] = useState<Task[]>([
     { id: 1, name: "Criar o app", completed: true} // example task
   ])
+  const [taskInput, setTaskInput] = useState<string>("")
 
   return (
     <>
-      <h2>Tasks</h2>
-      <ul className='mainTaskList'>
+      <div className='task-input'>
+        <h2>Tasks</h2>
+        <input className='task-input-field' onChange={(event) => (setTaskInput(event.target.value))} value={taskInput}/>
+        <button>Adicionar</button>
+      </div>
+
+      <ul className='main-task-list'>
         {
           taskList.map((currentTask) => (
             <li key={currentTask.id}>{currentTask.name} | status: {currentTask.completed ? "completo" : "incompleto"}</li>
