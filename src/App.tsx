01238a66473e0ceebc5d8d4db74bb3401406c237
gabriel-pinotti <gabriel.apinotti06@gmail.com>
2026-07-10 19:37:49 -0300

    feat: checkboxes and task status

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,40 @@ import type { Task } from './types/task'
 
 function App() {
   const [taskList, setTaskList] = useState<Task[]>([])
-  const [taskInput, setTaskInput] = useState<string>("")
+  const [taskInputField, setTaskInputField] = useState<string>("")
 
   function handleConfirmListAdd() {
-    setTaskList([ ...taskList, {id: Date.now(), name: taskInput, completed: false}])
-    setTaskInput("")
+    setTaskList([ ...taskList, {id: Date.now(), name: taskInputField, completed: false}])
+    setTaskInputField("")
+  }
+
+  function handleTaskCheck(id: number) {
+    setTaskList(
+      taskList.map((task) => (
+        task.id === id
+          ? {...task, completed: !task.completed}
+          : task
+      ))
+    )
   }
 
   return (
     <>
       <div className='task-input'>
         <h2>Tasks</h2>
-        <input className='task-input-field' onChange={(event) => (setTaskInput(event.target.value))} value={taskInput}/>
+        <input className='task-input-field' onChange={(event) => (setTaskInputField(event.target.value))} value={taskInputField}/>
         <button onClick={handleConfirmListAdd}>Adicionar</button>
       </div>
 
       <ul className='main-task-list'>
         {
           taskList.map((currentTask) => (
-            <li key={currentTask.id}>{currentTask.name} | status: {currentTask.completed ? "completo" : "incompleto"}</li>
+            <li key={currentTask.id}>
+              <input type='checkbox' checked={currentTask.completed} id={"task-checkbox" + currentTask.id} onChange={() => handleTaskCheck(currentTask.id)}/>
+              <label htmlFor={"task-checkbox" + currentTask.id}>
+                {currentTask.name} | status: {currentTask.completed ? "completo" : "incompleto"}
+              </label>
+            </li>
           ))
         }
       </ul>
