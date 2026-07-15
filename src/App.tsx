598785084d296505134f220761b620f4dbd2e399
gabriel-pinotti <gabriel.apinotti06@gmail.com>
2026-07-15 16:13:31 -0300

    feat: delete button

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,13 @@ function App() {
     setTaskList([ ...taskList, {id: Date.now(), name: taskInputField, completed: false}])
     setTaskInputField("")
   }
-
+  
+  function handleDeleteTask(id: number) {
+    setTaskList(
+      taskList.filter((task) => task.id !== id)
+    )
+  }
+  
   function handleTaskCheck(id: number) {
     setTaskList(
       taskList.map((task) => (
@@ -36,6 +42,7 @@ function App() {
               <label htmlFor={"task-checkbox" + currentTask.id}>
                 {currentTask.name} | status: {currentTask.completed ? "completo" : "incompleto"}
               </label>
+              <button onClick={() => handleDeleteTask(currentTask.id)} >Deletar</button>
             </li>
           ))
         }
